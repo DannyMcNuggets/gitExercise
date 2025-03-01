ac9a3006bd37256f02971037446bd5363ba2a2bb
DannyMcNuggets <dannymcwhopper@gmail.com>
2025-03-01 09:46:21 +0200

    'friend' did smthng

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("goodybe world");
+        System.out.println("friend (or actually just me from another machine) was heres");
 
     }
 }
